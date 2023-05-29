c81d4b670eb53230f7263555dc0227d547e15e4a
nwankwolinus <51293362+nwankwolinus@users.noreply.github.com>
2023-05-29 08:45:56 +0100

    My first Commit

---
+++ exercise1.2/1.2/src/components/Content.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Part from './Part'
+
+const Content = ({name}) => {
+    const part1 = 'Fundamentals of React'
+    const exercises1 = 10
+    const part2 = 'Using props to pass data'
+    const exercises2 = 7
+    const part3 = 'State of a component'
+    const exercises3 = 14
+
+    return (
+        <div>
+            <Part name={part1} no_of_exercise={exercises1} />
+            <Part name={part2} no_of_exercise={exercises2} />
+            <Part name={part3} no_of_exercise={exercises3} />
+        </div>
+        )
+    }
+
+export default Content;
